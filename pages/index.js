04d654e26b75372ecd8642f8eefb3540cfcc8a79
Htmng17 <guiix98@gmail.com>
2026-03-02 15:57:07 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <><h1>Davi é tchola</h1>
+    <h2>Eu acho que o Davi é muito boiola</h2>
+    <h3>Davi tem tido muitas atitudes homosexuais, talvez ele goste de liberar o boga</h3></>
+}
+
+export default Home;
