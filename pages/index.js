5598d5fc3b3b6fc52a9de98437030687fe246c7c
Htmng17 <guiix98@gmail.com>
2026-03-02 20:31:36 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <><h1>Davi é tchola</h1>
+    return <><h1>Davi é viadum</h1>
     <h2>Eu acho que o Davi é muito boiola</h2>
     <h3>Davi tem tido muitas atitudes homosexuais, talvez ele goste de liberar o boga</h3></>
 }
